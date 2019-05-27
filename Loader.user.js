ac6559ad6d35d67b25126fc16960d6b7a9a8255d
Polfy <46058408+Polfy@users.noreply.github.com>
2019-05-27 23:50:28 +0200

    Create Loader.user.js

---
+++ Loader.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Isleward - Addon Bundle Loader
+// @namespace    Isleward.Addon
+// @version      0.3
+// @description  Loads Addon Bundle
+// @author       Qndel
+// @match        play.isleward.com*
+// @grant        none
+// ==/UserScript==
+var d=a=>window.$?$.getScript`https://polfy.github.io/IsleWaddonWeb/IsleWaddonWeb.js`:setTimeout(d,50);d()
