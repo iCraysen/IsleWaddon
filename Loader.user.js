52918852106f79f9c7f04f1376d7bdebc2f38930
Polfy <46058408+Polfy@users.noreply.github.com>
2019-05-28 00:47:06 +0200

    Update Loader.user.js

--- Loader.user.js
+++ Loader.user.js
@@ -1,9 +1,8 @@
 // ==UserScript==
-// @name         Isle Waddon
-// @version      3.3
-// @description  Check readme : https://github.com/Polfy/IsleWaddon
+// @name         IsleWaddon
+// @version      0.1
+// @description  blahblah
 // @author       Polfy's
-// @match        https://play.isleward.com/
+// @match        play.isleward.com*
 // ==/UserScript==
-
-var d=a=>window.$?$.getScript`https://polfy.github.io/IsleWaddon/IsleWaddon.js`:setTimeout(d,50);d()
+var d=a=>window.$?$.getScript`https://Polfy.github.io/IsleWaddon/IsleWaddon.js`:setTimeout(d,50);d()
