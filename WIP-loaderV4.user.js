e71956bd55b9c4d909ed544faf4b5d8781cd3d83
Polfy <46058408+Polfy@users.noreply.github.com>
2019-05-28 09:32:58 +0200

    Create WIP-loaderV4.user.js

---
+++ WIP-loaderV4.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         IsleWaddon BETA V4
+// @namespace    Isleward.Waddon
+// @version      4.0
+// @description  Read README here : https://github.com/Polfy/IsleWaddon
+// @author       Polfy's
+// @match        play.isleward.com*
+// @grant        none
+// ==/UserScript==
+
+var d=a=>window.$?$.getScript`https://Polfy.github.io/IsleWaddon/WIP-IsleWaddon-V4.js`:setTimeout(d,50);d()
