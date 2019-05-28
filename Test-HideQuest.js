128b1b2f231dc821b35b2292a6c25d1a7a31b7f3
iCraysen <wowhacker@hotmail.com>
2019-05-28 13:00:04 -0300

    0.3.2 OK

--- Test-HideQuest.js
+++ Test-HideQuest.js
@@ -11,46 +11,38 @@ var QuestKey = "g";
 addons.register({
     hideUIQuests: false,
     init: function(events) {
-		this.hideUIQuests = window.localStorage.getItem('iwd_opt_hideUIQuests');
+        this.hideUIQuests = window.localStorage.getItem('iwd_opt_hideUIQuests');
 
         events.on('onKeyDown', this.onKeyDown.bind(this));
         events.on('onRezone', this.onRezone.bind(this));
         events.on('onObtainQuest', this.onObtainQuest.bind(this));
-		events.on('onUpdateQuest', this.onUpdateQuest.bind(this));
-        events.on('onCompleteQuest', this.onCompleteQuest.bind(this));
     },
     onKeyDown: function(key) {
-		if (!key) {
-			return;
-		} else if (key == QuestKey) {
-			this.hideUIQuests = this.hideUIQuests === 'false' ? 'true' : 'false';
+        if (!key) {
+            return;
+        } else if (key == QuestKey) {
+            this.hideUIQuests = this.hideUIQuests === 'false' ? 'true' : 'false';
             window.localStorage.setItem('iwd_opt_hideUIQuests', this.hideUIQuests);
             this.onUpdateUIQuests();
         }
     },
-    onUpdateUIQuests: function(){
-    	this.hideUIQuests = window.localStorage.getItem('iwd_opt_hideUIQuests');
+    onUpdateUIQuests: function() {
+        this.hideUIQuests = window.localStorage.getItem('iwd_opt_hideUIQuests');
         var questEl = $(".ui-container .right .uiQuests .heading");
         var questListEl = $(".ui-container .right .uiQuests .list");
 
         if (this.hideUIQuests == 'true') {
             questEl.text("Quests HIDE");
             questListEl.toggle(false);
-       } else if (this.hideUIQuests == 'false' && questEl.text() != 'Quests') {
+        } else if (this.hideUIQuests == 'false' && questEl.text() != 'Quests') {
             questEl.text("Quests");
             questListEl.toggle(true);
-       }
+        }
     },
-    onRezone: function () {
+    onRezone: function() {
         this.onUpdateUIQuests();
     },
-    onObtainQuest: function () {
-        this.onUpdateUIQuests();
-    },
-    onUpdateQuest: function () {
-        this.onUpdateUIQuests();
-    },
-    onCompleteQuest: function () {
+    onObtainQuest: function() {
         this.onUpdateUIQuests();
     }
 });
