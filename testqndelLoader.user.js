121f7104f2ecb26c3211db06124927ea0d303d04
Polfy <46058408+Polfy@users.noreply.github.com>
2019-05-28 07:20:49 +0200

    Create testqndelLoader.user.js

---
+++ testqndelLoader.user.js
@@ -0,0 +1,18 @@
+    
+// ==UserScript==
+// @name         Isleward - Addon Bundle Loader
+// @namespace    Isleward.Addon
+// @version      0.3
+// @description  Loads Addon Bundle
+// @author       Qndel
+// @match        play.isleward.com*
+// @grant        none
+// ==/UserScript==
+var d=a=>window.$?$.getScript`https://qndel.github.io/IslewardAddonBundle/core.js`:setTimeout(d,50);d()
+
+//version to paste in console if not using tampermonkey:
+/*
+$.getScript`https://qndel.github.io/IslewardAddonBundle/core.js`;
+wait for a second then
+window.loadAddonBundlePanel();
+*/
