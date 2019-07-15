ad70b5ce91051bd93b4a23935edfb7826e1745e7
Polfy <46058408+Polfy@users.noreply.github.com>
2019-07-15 22:38:40 +0200

    Update RuneData.js

--- IsleWaddonFeature/ToolTipExpand/RuneData.js
+++ IsleWaddonFeature/ToolTipExpand/RuneData.js
@@ -1,12 +1,14 @@
 window.spells = {
     'magic missile': {
         statType: 'int',
+        spellType : 'Arcane Spell',
         random: {
             damage: [4, 15]
         }
     },
     'ice spear': {
         statType: 'int',
+        spellType : 'Frost Spell',
         random: {
             damage: [2, 8],
             i_freezeDuration: [6, 10]
@@ -14,6 +16,7 @@ window.spells = {
     },
     fireblast: {
         statType: 'int',
+        spellType : 'Fire Spell',
         random: {
             damage: [2, 5],
             i_radius: [1, 2],
@@ -22,6 +25,7 @@ window.spells = {
     },
     smite: {
         statType: 'int',
+        spellType : 'Holy Spell',
         random: {
             damage: [4, 14],
             i_stunDuration: [6, 10]
@@ -29,6 +33,7 @@ window.spells = {
     },
     consecrate: {
         statType: 'int',
+        spellType : 'Holy Spell',
         random: {
             healing: [0.3, 0.5],
             i_duration: [7, 13]
@@ -37,12 +42,14 @@ window.spells = {
 
     slash: {
         statType: 'str',
+        spellType : 'Physical Attack',
         random: {
             damage: [6, 23]
         }
     },
     charge: {
         statType: 'str',
+        spellType : 'Physical Attack',
         random: {
             damage: [2, 8],
             i_stunDuration: [6, 10]
@@ -50,12 +57,14 @@ window.spells = {
     },
     flurry: {
         statType: 'none',
+        spellType : 'none',
         random: {
             i_duration: [4, 9]
         }
     },
     smokebomb: {
         statType: 'dex',
+        spellType : 'Poison Attack',
         random: {
             damage: [0.25, 1.45],
             i_radius: [1, 3],
@@ -64,28 +73,32 @@ window.spells = {
     },
     'crystal spikes': {
         statType: ['dex', 'int'],
+        spellType : 'Spell',
         random: {
             damage: [3, 14],
-            i_delay: [4, 1] // Real value [1, 4]
+            i_delay: [4, 1]   // Real value [1, 4]
         },
         negativeStats: [
             'i_delay'
         ]
     },
     innervation: {
-        statType: ['none'],
+        statType: 'none',
+        spellType : 'Aura Buff',
         random: {
             regenPercentage: [0.3, 1.5]
         }
     },
     tranquility: {
-        statType: ['none'],
+        statType: 'none',
+        spellType : 'Aura Buff',
         random: {
             regenPercentage: [4, 10]
         }
     },
     swiftness: {
-        statType: ['none'],
+        statType: 'none',
+        spellType : 'Aura Buff',
         random: {
             chance: [5, 10]
         }
@@ -93,6 +106,7 @@ window.spells = {
 
     'harvest life': {
         statType: ['str', 'int'],
+        spellType : 'Physical Attack',
         random: {
             damage: [4, 14],
             healPercent: [10, 30]
@@ -101,6 +115,7 @@ window.spells = {
 
     'summon skeleton': {
         statType: ['str', 'int'],
+        spellType : 'Spell',
         random: {
             damagePercent: [20, 76],
             hpPercent: [40, 60]
@@ -109,6 +124,7 @@ window.spells = {
 
      'blood barrier': {
          statType: ['str', 'int'],
+         spellType : 'Spell',
          random: {
              i_drainPercentage: [10, 50],
              shieldMultiplier: [2, 5],
